af88b1f44988ee5665ebac1f89cb137dda5a1550
gib <G_B_@mail.ru>
2023-08-19 13:01:48 +0300

    create project

---
+++ app/src/main/java/local/zva/hw3/MainActivity.kt
@@ -0,0 +1,16 @@
+package local.zva.hw3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickToast() {
+        Toast.
+    }
+}
