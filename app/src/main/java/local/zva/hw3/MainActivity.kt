2b4d0dcf49e6fb6c7880c9b4d0689f9a57a23122
gib <G_B_@mail.ru>
2023-08-21 14:15:15 +0300

    stage 2 card view, color graduation, round corners...

--- app/src/main/java/local/zva/hw3/MainActivity.kt
+++ app/src/main/java/local/zva/hw3/MainActivity.kt
@@ -1,16 +1,14 @@
 package local.zva.hw3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
-    fun onClickToast() {
-        Toast.
-    }
+
 }
