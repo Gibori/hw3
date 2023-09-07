e19c9495773746d707679d0fdc905cd2812b70a8
gib <G_B_@mail.ru>
2023-09-07 23:29:30 +0300

    stage 2.3.1 material view, toolbar menu Toast!!!

--- app/src/main/java/local/zva/hw3/MainActivity.kt
+++ app/src/main/java/local/zva/hw3/MainActivity.kt
@@ -5,11 +5,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import local.zva.hw3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMA: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMA = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMA.root)
+        bindingMA.topAppBar.findViewById<View>(R.id.settings).setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
