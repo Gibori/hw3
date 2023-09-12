856953cc566a12c06b3128a230150dded9174f89
gib <G_B_@mail.ru>
2023-09-12 22:01:13 +0300

    stage 2.3.2 material view, toolbar menu Toast Night Mode

--- app/src/main/java/local/zva/hw3/MainActivity.kt
+++ app/src/main/java/local/zva/hw3/MainActivity.kt
@@ -1,10 +1,10 @@
 package local.zva.hw3
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import local.zva.hw3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +13,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingMA = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMA.root)
-        bindingMA.topAppBar.findViewById<View>(R.id.settings).setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        bindingMA.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nightMode -> {
+                    nightModeChange()
+                    true
+                }
+                else -> false
+            }
+        }
+        bindingMA.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
 
     }
 
-    fun onClickBtn(view: View) {
-        val b = view as Button
-        Toast.makeText(this, b.text, Toast.LENGTH_SHORT).show()
+    private fun nightModeChange() {
+        if (isNightModeOn()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
     }
+
+    private fun isNightModeOn(): Boolean {
+        val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return nightModeFlags == Configuration.UI_MODE_NIGHT_YES
+    }
+
 
 }
