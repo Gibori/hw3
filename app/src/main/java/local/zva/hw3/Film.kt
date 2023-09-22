9d612884ccc4f73fb9a2f5e6119031c8c02e44b3
gib <G_B_@mail.ru>
2023-09-22 11:27:41 +0300

    stage 3 material view, toolbar menu Toast Night Mode //3 RV in center

--- app/src/main/java/local/zva/hw3/Film.kt
+++ app/src/main/java/local/zva/hw3/Film.kt
@@ -1,7 +1,11 @@
 package local.zva.hw3
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Film (
     val title: String,
     val poster: Int,
     val description: String
-)
+) : Parcelable
