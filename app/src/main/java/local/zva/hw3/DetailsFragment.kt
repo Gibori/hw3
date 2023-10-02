9c95b881498d9524408aa0acb771bb3ce14cb83c
gib <G_B_@mail.ru>
2023-10-02 22:12:22 +0300

    stage 5 делаем избранное реализуем кнопки избранное и шаре

--- app/src/main/java/local/zva/hw3/DetailsFragment.kt
+++ app/src/main/java/local/zva/hw3/DetailsFragment.kt
@@ -11,6 +11,7 @@ import local.zva.hw3.databinding.FragmentDetailsBinding
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    val film = arguments?.get("film") as Film
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,11 +25,24 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val film = arguments?.get("film") as Film
-
         binding.detailsToolbar.title = film.title
         binding.detailsDescription.text = film.description
         binding.detailsPoster.setImageResource(film.poster)
+
+        val favBtn = binding.detailsFabFavorites
+        favBtn.setImageResource(
+            if (film.isInFavorites) R.drawable.ic_baseline_favorite_24
+            else R.drawable.ic_baseline_favorite_border_24
+        )
+        favBtn.setOnClickListener {
+            if (film.isInFavorites) {
+                favBtn.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                film.isInFavorites = false
+            } else {
+                favBtn.setImageResource(R.drawable.ic_baseline_favorite_24)
+                film.isInFavorites = true
+            }
+        }
     }
 
 }
